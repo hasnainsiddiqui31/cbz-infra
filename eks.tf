ad36ffabde9e262654d70426fa5b82797c930d79
Hasnain Siddiqui <hasnainsiddiqui099@gmail.com>
2026-03-18 19:11:48 +0530

    Change instance type from t2.medium to t3.medium

---
+++ eks.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# VPC (simple)
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "cbz-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  enable_nat_gateway = false
+}
+
+# EKS Cluster
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0.0"
+
+  cluster_name    = "cbz-eks"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 1
+      max_size     = 2
+      min_size     = 1
+
+      instance_types = ["t3.medium"]
+    }
+  }
+}
